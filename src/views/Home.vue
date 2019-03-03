c133b2b65f26ea9d402b0415edaf5193596f3d1d
xiaoxiao1155 <10050692374@qq.com>
2019-03-03 11:03:25 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="btnClick">button</button>
+    <div>{{message}}</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        message: ""
+      }
+    },
+    methods:{
+      btnClick() {
+        this.message = "dfg"
+      }
+    }
   }
-}
 </script>
